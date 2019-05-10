020c72c76db9eeea183ba98de2666474e28955f8
miguelarman <miguelarman@gmail.com>
2019-05-10 19:06:35 +0200

    Comentarios de daemon

--- includes/daemon.h
+++ includes/daemon.h
@@ -1,3 +1,13 @@
+#ifndef DAEMON_H
+/**
+ * @file daemon.h
+ * @defgroup Daemon
+ *
+ * Esta librería se encarga de demonizar
+ */
+
+#define DAEMON_H
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,4 +18,16 @@
 
 #define ES_PADRE 5
 
+/**
+ * @brief Función que demoniza
+ *
+ * Esta función crea un proceso demonio y
+ * notifica al padre para que termine su ejecución
+ *
+ * @ingroup Daemon
+ * @return Código de si es el proceso padre o el demonio
+ */
 int demonizar(void);
+
+
+#endif
